c963193aa575f705aa8e98b64ab4002c04bfade0
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2024-12-10 19:45:25 +0000

    feat/; modify file to handle state and renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import FormDataContext from "./contexts/FormDataContext";
+import MultiStepForm from "../src/components/MultiStepForm/MultiStepForm";
+import { steps, SuccessPage } from "./steps";
+import { ObjectType } from "./type";
 
-
-const App: React.FC = () => {
-  return (
-      <FormDataContext /> 
-  );
+const initialValue = {
+	plan: "arcade",
+	billing: "Monthly",
+	addons: [],
 };
 
+function App() {
+	const handleSubmit = (data: ObjectType) => {
+		console.log(data);
+	};
+	return (
+		<main className="container">
+			<h1 className="sr-only">Multi Step Form</h1>
+			<MultiStepForm
+				steps={steps}
+				handleSubmit={handleSubmit}
+				initialValue={initialValue}
+				SuccessPage={SuccessPage}
+			/>
+		</main>
+	);
+}
 export default App;
---
+++ src/steps.ts
@@ -0,0 +1,28 @@
+import AddOns from "./components/Addons";
+import Info from "./components/Info";
+import { Step } from "./components/MultiStepForm/types";
+import Plan from "./components/Plan/Plan";
+import Success from "../src/components/Success/Success";
+import Summary from "./components/Summary/Summary";
+
+
+export const steps: Step[] = [
+	{
+		title: "YOUR INFO",
+		Component: Info,
+	},
+	{
+		title: "SELECT PLAN",
+		Component: Plan,
+	},
+	{
+		title: "ADD-ONS",
+		Component: AddOns,
+	},
+	{
+		title: "SUMMARY",
+		Component: Summary,
+	},
+];
+
+export const SuccessPage = Success;
