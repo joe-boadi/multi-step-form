5fd049f390d700d758465164fe5fb2aad32351ac
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2024-12-12 22:38:33 +0000

    feat: Create component for Addons for reusability

---
+++ src/components/Addons/AddOnItem.tsx
@@ -0,0 +1,24 @@
+import { AddOnProps } from './types';
+import styles from "../../assets/styles/Addons.module.css";
+
+export const AddOnItem = ({ addon, isSelected, billing, onSelect }: AddOnProps) => {
+    return (
+        <div
+            className={`${styles.addon} ${isSelected ? styles.selected : ''}`}
+            onClick={() => onSelect(addon.title)}
+        >
+            <input
+                type="checkbox"
+                checked={isSelected}
+                onChange={() => onSelect(addon.title)}
+            />
+            <div>
+                <h4>{addon.title}</h4>
+                <p>{addon.desc}</p>
+            </div>
+            <p className={styles.price}>
+                +${addon.price[billing]}/{billing === "Monthly" ? "mo" : "yr"}
+            </p>
+        </div>
+    );
+};
---
+++ src/components/Addons/Addons.tsx
@@ -0,0 +1,39 @@
+import { ComponentPropsType } from "../../type";
+import { addons } from "../../data";
+import styles from "../../assets/styles/Addons.module.css";
+import { AddOnItem } from "./AddOnItem";
+
+const AddOns = ({ data, setData }: ComponentPropsType) => {
+    const handleAddonClick = (addonTitle: string) => {
+        setData((prevState) => {
+            const newAddons = prevState.addons.includes(addonTitle)
+                ? prevState.addons.filter((existingAddon) => existingAddon !== addonTitle)
+                : [...prevState.addons, addonTitle];
+
+            return {
+                ...prevState,
+                addons: newAddons,
+            };
+        });
+    };
+
+    return (
+        <form className={styles.form}>
+            <h2>Pick add-ons</h2>
+            <p>Add-ons help to enhance your gaming experience.</p>
+            <div className={styles.addons}>
+                {addons.map(addon => (
+                    <AddOnItem
+                        key={addon.title}
+                        addon={addon}
+                        isSelected={data.addons.includes(addon.title)}
+                        billing={data.billing}
+                        onSelect={handleAddonClick}
+                    />
+                ))}
+            </div>
+        </form>
+    );
+};
+
+export default AddOns;
