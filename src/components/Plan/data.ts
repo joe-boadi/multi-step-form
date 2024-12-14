21098eaef27b410b075f53895747a9abb025c433
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2024-12-14 19:52:19 +0000

    modify code to access icon with correct path

--- src/components/Plan/data.ts
+++ src/components/Plan/data.ts
@@ -1,31 +1,25 @@
-
 export const PlanData = () => {
-
-
     return [
         {
             id: 'arcade',
             title: 'Arcade',
             price: { Monthly: 9, Yearly: 90 },
-            icon: '../../assets/images/icon-pro.svg',
+            icon: '/public/images/icon-arcade.svg',
             benefit: '2 months free'
-
         },
         {
             id: 'advanced',
             title: 'Advanced',
             price: { Monthly: 12, Yearly: 120 },
-            icon: '../../assets/images/icon-advanced.svg',
+            icon: '/public/images/icon-advanced.svg',
             benefit: '2 months free'
         },
         {
             id: 'pro',
             title: 'Pro',
             price: { Monthly: 15, Yearly: 150 },
-            icon: '../../assets/images/icon-pro.svg',
+            icon: '/public/images/icon-pro.svg',
             benefit: '2 months free'
         },
-    
     ];
 }
-
